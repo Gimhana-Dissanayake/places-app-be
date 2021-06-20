8a82f503665d6485ae4053dc8dc6848843bf7ae5
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-20 08:05:13 +0530

    93.adding controllers

---
+++ controllers/places-controller.js
@@ -0,0 +1,45 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrappers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404); // can only be used with sync code, not with async code
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    ); //can be used with both async and sync code.
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,48 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesController = require("../controllers/places-controller");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrappers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesController.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404); // can only be used with sync code, not with async code
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    ); //can be used with both async and sync code.
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesController.getPlaceByUserId);
 
 module.exports = router;
